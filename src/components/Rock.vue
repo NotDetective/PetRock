1bd63fce1fc6b16a4a34270aae1024e5a7339258
Woedroe <118165188+WoutLar@users.noreply.github.com>
2024-07-01 11:23:53 +0200

    added rock + coins

---
+++ src/components/Rock.vue
@@ -0,0 +1,30 @@
+<!-- src/components/Rock.vue -->
+<template>
+  <div class="rock-container">
+    <img src="@/assets/pet-rock.png" alt="Pet Rock" @click="incrementCoins" class="rock" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Rock",
+  methods: {
+    incrementCoins() {
+      this.$emit('increment');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.rock-container {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.rock {
+  width: 200px;
+  height: auto;
+  cursor: pointer;
+}
+</style>
