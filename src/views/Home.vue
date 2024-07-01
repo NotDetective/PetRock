11a3410844d40fb04dfd3b5d93d3329d68765c25
Woedroe <118165188+WoutLar@users.noreply.github.com>
2024-07-01 12:40:14 +0200

    shop correctly implemented

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,25 +4,66 @@
     <h1>Pet Rock Clicker</h1>
     <p>Coins: {{ coins }}</p>
     <Rock @increment="incrementCoins" />
+    <button @click="openShop">Open Shop</button>
+    <Shop :coins="coins" @buy-upgrade="buyUpgrade" ref="shop" />
   </div>
 </template>
 
 <script>
 import Rock from "@/components/Rock.vue";
+import Shop from "@/components/Shop.vue";
 
 export default {
   name: "Home",
   components: {
-    Rock
+    Rock,
+    Shop
   },
   data() {
     return {
-      coins: 0
+      coins: 0,
+      baseCoinValue: 1,
+      multiplier: 1, // Start with a base multiplier of 1
+      autoclickerInterval: null,
     };
   },
   methods: {
     incrementCoins() {
-      this.coins += 1;
+      this.coins += this.baseCoinValue * this.multiplier;
+    },
+    openShop() {
+      this.$refs.shop.openShop();
+    },
+    buyUpgrade(upgrade) {
+      if (this.coins >= upgrade.cost) {
+        this.coins -= upgrade.cost;
+        if (upgrade.type === "multiplier") {
+          this.multiplier += upgrade.multiplier; // Correctly add to the multiplier
+        } else if (upgrade.type === "autoclicker") {
+          this.activateAutoclicker(upgrade.value);
+        } else if (upgrade.type === "music") {
+          this.playMusic(upgrade.value);
+        }
+        // Handle more upgrade types here
+      }
+    },
+    activateAutoclicker(clicksPerSecond) {
+      if (this.autoclickerInterval) {
+        clearInterval(this.autoclickerInterval);
+      }
+      this.autoclickerInterval = setInterval(() => {
+        this.incrementCoins();
+      }, 1000 / clicksPerSecond);
+    },
+    playMusic(file) {
+      const audio = new Audio(file);
+      audio.loop = true;
+      audio.play();
+    }
+  },
+  beforeDestroy() {
+    if (this.autoclickerInterval) {
+      clearInterval(this.autoclickerInterval);
     }
   }
 };
@@ -40,5 +81,11 @@ h1 {
 
 p {
   font-size: 1.5em;
+}
+
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  cursor: pointer;
 }
 </style>
